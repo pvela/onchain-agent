227b138b0d617e300aa90bb4434d2cb56ace7db2
Jarrod Watts <jarrodwattsyt@gmail.com>
2024-11-29 18:11:41 +1100

    OpenAI Hello World

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import 'dotenv/config';
+import OpenAI from "openai";
+
+const openai = new OpenAI();
+
+const completion = await openai.chat.completions.create({
+    model: "gpt-4o",
+    messages: [
+        { "role": "user", "content": "write a haiku about ai" }
+    ]
+});
+
+console.log(completion.choices[0].message.content);
